b5d35949a963bd012698fd8187ba6e3e107cf38b
boomerng <mrrochen@gmail.com>
2019-04-28 07:46:21 -0400

    Initial working

--- src/App.js
+++ src/App.js
@@ -4,12 +4,34 @@ import utils from './utils';
 import './App.css';
 
 const App = () => {
-  const [cards, setCards] = useState(utils.random(1, 5));
+  const cards = [2,1,3,1,2,3];
+  const [cardPick, setCardPick] = useState(-1);
+  const [clickedCard, setClickedCard] = useState(-1);
+  const [pickedCards, setPickedCards] = useState([]);
 
   return (
     <div className="App">
-      {utils.range(1, cards).map(c => 
-        <h1 key={c} className="h1">{c}</h1>
+      {cards.map((c, i) => 
+        <h1 key={i} className="h1"
+          onMouseDown={() => {
+            setClickedCard(i);
+          }}
+          onMouseUp={() => {
+            setClickedCard(-1);
+          }}
+          onClick={() => { 
+            if (cardPick === -1) {
+              setCardPick(i);
+            } else {
+              if (cards[cardPick] === c) {
+                setPickedCards(pickedCards.concat([cardPick, i]));
+              }
+              setCardPick(-1);
+            }
+          }}
+          >
+          {pickedCards.indexOf(i) > -1 || cardPick === i || clickedCard === i ? c: 'X'}
+        </h1>
       )}
     </div>
   );
--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,7 @@ const utils = {
     range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
   
     // pick a random number between min and max (edges included)
-    random: (min, max) => min + Math.floor(max * Math.random() * 2),
+    random: (min, max) => min + Math.floor(max * Math.random()) * 2,
   
     // Given an array of numbers and a max...
     // Pick a random sum (< max) from the set of all available sums in arr
