c4d684c6437b7204e69ef3a3282dea5e074f7b99
boomerng <mrrochen@gmail.com>
2019-04-28 20:51:16 -0400

    Final commit

--- src/endGame.js
+++ src/endGame.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const EndGame = props => (
+    <>
     <h1>
-        {props.lost ? "You lose" : "You win"}
+        {props.lost ? "You lose!" : "You win!"}
     </h1>
+    <button onClick={props.playAgain}>Play Again</button>
+    </>
 );
 
 export default EndGame
---
+++ src/gameWrapper.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import Card from './Card';
+import EndGame from './endGame';
+import utils from './utils';
+
+import './gameWrapper.css';
+
+const cards = utils.duplicate(utils.range(1, 8)).sort(() => Math.random() - 0.5);
+
+const GameWrapper = (props) => {
+  const [cardPick, setCardPick] = useState(-1);
+  const [clickedCard, setClickedCard] = useState(-1);
+  const [pickedCards, setPickedCards] = useState([]);
+  const [timeLeft, setTimeLeft] = useState(120);
+  
+  useEffect(() => {
+    if (timeLeft > 0 && pickedCards.length !== cards.length) {
+      const timerId = setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
+      return () => clearTimeout(timerId);
+    }
+  });
+
+  const isShowCard = (cardIndex) => {
+    return pickedCards.includes(cardIndex) || cardPick === cardIndex || clickedCard === cardIndex;
+  };
+
+  const onCardMouseToggle = (cardIndex) => {
+    setClickedCard(cardIndex);
+  }
+
+  const onCardClick = (cardIndex, card) => {
+    if (cardPick === -1) {
+      setCardPick(cardIndex);
+    } else {
+      if (cards[cardPick] === card && cardIndex !== cardPick) {
+        setPickedCards(pickedCards.concat([cardPick, cardIndex]));
+      }
+      setCardPick(-1);
+    }
+  }
+
+  const gameIsLost = timeLeft === 0;
+  const gameIsWon = pickedCards.length === cards.length;
+
+  return (
+    <div className="App">
+      {(gameIsLost || gameIsWon) ? 
+      <EndGame lost={gameIsLost} playAgain={props.resetClick} />
+      :
+      (<>
+        <div className="Card-container">
+          {cards.map((c, i) => 
+            <Card key={i} 
+              card={c}
+              cardIndex={i}
+              isShowCard={isShowCard} 
+              onCardMouseToggle={onCardMouseToggle}
+              onCardClick={onCardClick}
+            />
+          )}
+        </div>
+        <div className="Margin-Center">
+          Time Remaining: {timeLeft}
+        </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default GameWrapper;
